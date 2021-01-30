9ea943234a21b608e2e6fd899fb53e9b87b2c07c
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2021-01-30 12:32:29 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+var results=[]; var timing=[];
+var timer_c=0;
+var correcter=0; var incorrect=0;
+var final;
+$('#quoteInput').bind('copy paste', function (e) {
+  e.preventDefault();
+});
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
+
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+      correcter++;
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      incorrect++;
+      correct = false
+    }
+  })
+
+  if (correct) 
+  {
+    renderNewQuote()
+    final=correcter/(incorrect+correcter);
+    console.log();
+    results.push(final);
+    timing.push(timer);
+    correcter=0;  incorrect=0;
+    var x= Math.max(...results)
+    x=x*100;
+    var x1= Math.min(...timerElement)
+    document.getElementById("max_time").innerHTML =`Best Accuracy ${x}% and Best Timing is ${x1} `;
+  }
+})
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
+}
+
+async function renderNewQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
+}
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
+
+renderNewQuote()
